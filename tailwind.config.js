6fda0933995477854b01b9492d4c6af5bf054230
petarstankov <petarstankov@2create.studio>
2024-01-30 12:32:20 +0200

    https://bannerwave.monday.com/boards/5886039676/pulses/5955656217: fix buttons shadow

--- tailwind.config.js
+++ tailwind.config.js
@@ -214,7 +214,7 @@ module.exports = {
       },
       boxShadow: {
         'shadow-1': '0px 4px 4px 0px rgba(0, 0, 0, 0.25)',
-        'shadow-button': '0px 0px 0px .2px #39B077',
+        'shadow-button': '0px 0px 0px 1px #39B077',
         'shadow-bottom-green': '0px 2px 0px 0px #39B077',
       },
       aspectRatio: {
